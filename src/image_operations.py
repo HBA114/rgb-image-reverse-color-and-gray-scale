c4aca551406cc7882c77c871dffb0e40960b2a35
HBA114 <hbasriayhaner114@gmail.com>
2022-10-13 14:42:27 +0300

    For loop for red, green and blue parts of pixel

--- src/image_operations.py
+++ src/image_operations.py
@@ -11,9 +11,10 @@ def convert_to_gray_scale(image):
             mean = 0
             for pixelVal in image[i][j]: sum += pixelVal
             mean = int(sum / len(image[i][j]))
-            gray_scale_image[i][j][0] = mean
-            gray_scale_image[i][j][1] = mean
-            gray_scale_image[i][j][2] = mean
+
+            # k is index of red green and blue in current pixel
+            for k in range(len(image[i][j])):
+                gray_scale_image[i][j][k] = mean
     
     return gray_scale_image
 
@@ -27,9 +28,9 @@ def convert_to_reverse(image):
 
     for i in range(height):
         for j in range(width):
-            x = 255
-            reverse_image[i][j][0] = x - image[i][j][0]
-            reverse_image[i][j][1] = x - image[i][j][1]
-            reverse_image[i][j][2] = x - image[i][j][2]
+            # k is index of red green and blue in current pixel
+            # 255 is max pixel value and it is white
+            for k in range(len(image[i][j])):
+                reverse_image[i][j][k] = 255 - reverse_image[i][j][k]
 
     return reverse_image
